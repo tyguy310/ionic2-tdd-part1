2a3723d71cfa6091335f7fd9f004d5eb335c782e
tyguy310 <tyler_maier@yahoo.com>
2017-02-12 21:42:29 -0700

    product page displays mock data

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Products } from '../../providers/products';
+import { Products } from '../../providers/products'
 
-/*
-  Generated class for the Product page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-product',
   templateUrl: 'product.html'
@@ -17,7 +11,7 @@ export class ProductPage {
   constructor(public navCtrl: NavController, public productsService: Products) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductPage');
+    this.productsService.load();
   }
 
 }
